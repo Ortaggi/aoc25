751fff3b873ff9284d189697852af79b115354f1
Simone <sbaglivi@gmail.com>
2024-12-10 11:22:20 +0100

    first part done

--- d10/main.py
+++ d10/main.py
@@ -7,31 +7,33 @@ def search_path(grid, cury, curx):
         peak = set()
         peak.add((cury, curx))
         return peak
-    miny = cury - 1 if cury - 1 >= 0 else cury
-    maxy = cury + 1 if cury + 1 < line_len else cury
-    minx = curx - 1 if curx - 1 >= 0 else curx
-    maxx = curx + 1 if curx + 1 < line_count else curx
+    to_check = []
+    if cury - 1 >= 0:
+        to_check += [(cury-1, curx)]
+    if cury + 1 < line_count:
+        to_check += [(cury+1, curx)]
+    if curx - 1 >= 0:
+        to_check += [(cury, curx - 1)]
+    if curx + 1 < line_len:
+        to_check += [(cury, curx + 1)]
     peaks = set()
-    for y in range(miny, maxy + 1):
-        for x in range(minx, maxx + 1):
-            if y == cury and x == curx:
-                continue
-            if grid[y][x] != v + 1:
-                continue
-            # print(f"{v=} {cury=} {curx=} {y=} {x=}")
-            peaks = peaks.union(search_path(grid, y, x))
-    # print(peaks)
+    for point in to_check: 
+        y,x = point
+        if grid[y][x] != v + 1:
+            continue
+        # print(f"{v=} {cury=} {curx=} {y=} {x=}")
+        peaks = peaks.union(search_path(grid, y, x))
     return peaks
 
 def main():
     global line_count, line_len
-    test = True
+    test = False
     fname = "test.txt" if test else "real.txt"
     with open(fname) as f:
         content = f.read()
     lines = [[int(x) for x in l] for l in content.split('\n') if l.strip()]
-    for l in lines:
-        print(l)
+    # for l in lines:
+    #     print(l)
     line_count = len(lines)
     line_len = len(lines[0])
     answer = 0
@@ -41,7 +43,7 @@ def main():
             if c != 0:
                 continue
             peaks = search_path(lines, y, x)
-            print(y, x, peaks)
+            # print(y, x, peaks)
             answer += len(peaks)
             # print(answer, y, x)
     print(answer)
